0466019940676ab45177bb212df95c4499d98751
Rodion <redia1994@mail.ru>
2021-04-12 16:21:43 +0300

    finally

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -2,6 +2,7 @@ const images = () => {
     const imgPopup = document.createElement('div');
     const worksField = document.querySelector('.works');
     const img = document.createElement('img');
+    const scroll = calcScroll();
 
     imgPopup.classList.add('popup');
     worksField.appendChild(imgPopup);
@@ -15,12 +16,29 @@ const images = () => {
         e.preventDefault();
         if (e.target && e.target.classList.contains('preview')) {
             imgPopup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
             const path = e.target.parentNode.getAttribute('href');
             img.setAttribute('src', path);
         }
         if (e.target && e.target.classList.contains('popup')){
             imgPopup.style.display = 'none';
+            document.body.style.overflow = '';
+            document.body.style.marginRight = '0px';
         }
     });
+
+    function calcScroll() {
+        const div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'none';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+
+        div.remove();
+        return scrollWidth;
+    }
 };
 export default images;
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,6 +4,7 @@ const modals = () => {
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
         const windows = document.querySelectorAll('[data-modal]');
+        const scroll = calcScroll();
 
         triggers.forEach(trigger => {
             trigger.addEventListener('click', (e) => {
@@ -15,12 +16,14 @@ const modals = () => {
                 });
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
                 // document.body.classList.add('modal-open');
             });    
         });
         close.addEventListener('click', () => {
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
             windows.forEach(item => {
                 item.style.display = 'none';
             });
@@ -31,7 +34,8 @@ const modals = () => {
         modal.addEventListener('click', (e) => {
             if (e.target === modal && closeClickOverlay) {
                 modal.style.display = 'none';
-                document.body.style.overflow = '';  
+                document.body.style.overflow = ''; 
+                document.body.style.marginRight = `0px`;
                 windows.forEach(item => {
                     item.style.display = 'none';
                 });
@@ -45,6 +49,20 @@ const modals = () => {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = 'hidden';
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
     //showModalByTime('.popup', 60000);
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
